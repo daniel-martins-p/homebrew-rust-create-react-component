b52ce9c672769115afa85dadc38f901e0801714a
Daniel Martins <mp.daniel.414@gmail.com>
2023-04-16 22:09:53 -0300

    First release

---
+++ Formula/rust-create-react-component.rb
@@ -0,0 +1,11 @@
+class RustCreateReactComponent < Formula
+  desc "Create components for React.js with ease."
+  homepage "https://github.com/daniel-martins-p/rust-create-react-component"
+  url "https://github.com/daniel-martins-p/rust-create-react-component/releases/download/v0.1.0/create-react-component.tar.gz"
+  sha256 "e3fc21c26e2115f852ec69e1acb21c4e765acd24888712287d1bf8b46e2ba6d2"
+  version "0.1.0"
+
+  def install
+    bin.install "create-react-component"
+  end
+end
