b5b2e17a2bdd9b1ec2314f0c564f207ba7ef248d
Daniel Martins <mp.daniel.414@gmail.com>
2023-04-16 22:22:24 -0300

    Improve description

--- Formula/rust-create-react-component.rb
+++ Formula/rust-create-react-component.rb
@@ -1,5 +1,5 @@
 class RustCreateReactComponent < Formula
-  desc "Create components for React.js with ease."
+  desc "Create components for React.js with ease using a CLI create with Rust"
   homepage "https://github.com/daniel-martins-p/rust-create-react-component"
   url "https://github.com/daniel-martins-p/rust-create-react-component/releases/download/v0.1.0/create-react-component.tar.gz"
   sha256 "e3fc21c26e2115f852ec69e1acb21c4e765acd24888712287d1bf8b46e2ba6d2"
